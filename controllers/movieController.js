66c3847696f4a5f9f1c82b7e8a206b15bb64d30d
Reshmasri-yhs <23ht1a0509@gmail.com>
2026-07-18 23:36:32 +0530

    Refactor movie controller to use async/await

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,117 +1,164 @@
 import Movie from "../models/Movie.js";
+import mongoose from "mongoose";
 
-function getMovies(req,res){
-    Movie.find()
-    .then(function(movies){
-        res.status(200).json(movies);
-    })
-    .catch(function(error){
-        res.status(500).json({message:error.message});
-    });
-}
+export const getMovies = async(req,res)=>{
+    try{
 
+        const movies = await Movie.find();
 
-function getMovieById(req,res){
-    Movie.findById(req.params.id)
-    .then(function(movie){
+        res.status(200).json({
+            success:true,
+            count:movies.length,
+            movies
+        });
 
-        if(!movie){
-            return res.status(404).json({
-                message:"Movie not found"
-            });
-        }
+    }
+    catch(error){
 
-        res.status(200).json(movie);
-    })
-    .catch(function(error){
         res.status(500).json({
+            success:false,
             message:error.message
         });
-    });
-}
 
+    }
+};
 
-function addMovie(req,res){
+export const getMovieById = async(req,res)=>{
+    try{
 
-    const movie = new Movie({
-        title:req.body.title,
-        year:req.body.year,
-        genre:req.body.genre,
-        rating:req.body.rating,
-        language:req.body.language,
-        poster:req.body.poster
-    });
-
-
-    movie.save()
-    .then(function(newMovie){
-        res.status(201).json(newMovie);
-    })
-    .catch(function(error){
-        res.status(400).json({
-            message:error.message
-        });
-    });
-
-}
-
-
-function updateMovie(req,res){
-
-    Movie.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {new:true}
-    )
-    .then(function(movie){
-
-        if(!movie){
-            return res.status(404).json({
-                message:"Movie not found"
+        if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+            return res.status(400).json({
+                success:false,
+                message:"Invalid Movie ID"
             });
         }
 
-        res.status(200).json(movie);
-
-    })
-    .catch(function(error){
-        res.status(400).json({
-            message:error.message
-        });
-    });
-
-}
-
-
-function deleteMovie(req,res){
-
-    Movie.findByIdAndDelete(req.params.id)
-    .then(function(movie){
+        const movie = await Movie.findById(req.params.id);
 
         if(!movie){
             return res.status(404).json({
+                success:false,
                 message:"Movie not found"
             });
         }
 
         res.status(200).json({
+            success:true,
+            movie
+        });
+
+    }
+    catch(error){
+
+        res.status(500).json({
+            success:false,
+            message:error.message
+        });
+
+    }
+};
+
+
+export const addMovie = async (req,res)=>{
+    try{
+
+        const movie = await Movie.create(req.body);
+
+        res.status(201).json({
+            success:true,
+            movie
+        });
+
+    }
+    catch(error){
+
+        res.status(400).json({
+            success:false,
+            message:error.message
+        });
+
+    }
+};
+
+
+export const updateMovie = async(req,res)=>{
+    console.log("ID received:", req.params.id);
+
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+        return res.status(400).json({
+            success:false,
+            message:"Invalid Movie ID"
+        });
+    }
+
+    try{
+
+        const movie = await Movie.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new:true,
+                runValidators:true
+            }
+        );
+
+        if(!movie){
+            return res.status(404).json({
+                success:false,
+                message:"Movie not found"
+            });
+        }
+
+        res.status(200).json({
+            success:true,
+            message:"Movie updated successfully",
+            movie
+        });
+
+    }
+    catch(error){
+
+        res.status(400).json({
+            success:false,
+            message:error.message
+        });
+
+    }
+};
+
+
+export const deleteMovie = async(req,res)=>{
+
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+        return res.status(400).json({
+            success:false,
+            message:"Invalid Movie ID"
+        });
+    }
+
+    try{
+
+        const movie = await Movie.findByIdAndDelete(req.params.id);
+
+        if(!movie){
+            return res.status(404).json({
+                success:false,
+                message:"Movie not found"
+            });
+        }
+
+        res.status(200).json({
+            success:true,
             message:"Movie deleted successfully"
         });
 
-    })
-    .catch(function(error){
+    }
+    catch(error){
+
         res.status(500).json({
+            success:false,
             message:error.message
         });
-    });
 
-}
-
-
-export {
-    getMovies,
-    getMovieById,
-    addMovie,
-    updateMovie,
-    deleteMovie
+    }
 };
