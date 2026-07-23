71e0aca9d2b11cf9f0660893f981565428fd6c0a
Reshmasri-yhs <23ht1a0509@gmail.com>
2026-07-23 23:12:22 +0530

    Added routes for the favourites

---
+++ routes/favoriteRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+import {
+getFavorites,
+addFavorite,
+removeFavorite
+}
+from "../controllers/favoriteController.js";
+
+
+const router=express.Router();
+
+
+router.get("/:userId",getFavorites);
+
+router.post("/",addFavorite);
+
+router.delete("/",removeFavorite);
+
+
+export default router;
